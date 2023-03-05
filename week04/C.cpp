9ce13666cf9f803d23ab0b2020c13554fadd16f0
PaVVovk <vovk.pa@phystech.edu>
2023-03-06 00:46:48 +0300

    Cheops_sort remake

--- week04/C.cpp
+++ week04/C.cpp
@@ -14,9 +14,8 @@ void swap(int &a, int &b){
     b = tmp;
 }
 
-void triple_sort(int (&arr)[N], int i, int max_len){
-    //Сравнение родителя и двух детей в куче
-    //для сортировки по возрастанию
+void heap_sort(int (&arr)[N], int i, int max_len){
+    //для сортировки кучи по возрастанию
     int max = i;
     int left = 2*i + 1;
     int right = 2*i + 2;
@@ -27,19 +26,23 @@ void triple_sort(int (&arr)[N], int i, int max_len){
     if ((right < max_len) and (arr[max] < arr[right])){
         max = right;
     }
-    swap(arr[max], arr[i]);
+    if (max!=i){
+        swap(arr[max], arr[i]);
+        heap_sort(arr, max, max_len);
+    }
+    
 }
 
-void cheops_sort(int (&arr)[N], int len){
+void cheops_sort(int (&arr)[N]){
     //Сортировка по возрастанию
-    if (len < 2){
-        return;
+    for (int i = N/2; i>=0; i--){
+        heap_sort(arr, i, N);
     }
-    for (int i = len/2; i>=0; i--){
-        triple_sort(arr, i, len);
+    
+    for (int i = N-1; i>=0; i--){
+        swap(arr[0], arr[i]);
+        heap_sort(arr, 0, i);
     }
-    swap(arr[0], arr[len-1]);
-    cheops_sort(arr, len-1);
 }
 
 int main(){
@@ -47,7 +50,7 @@ int main(){
     for (int i = 0; i < N; i++){
         cin >> a[i];
     }
-    cheops_sort(a, N);
+    cheops_sort(a);
     for (int i = 0; i < N; i++){
         cout << a[i] << ' ';
     }
